de1b637977b2947f502f4e472f00ddcba681221a
Takahiro SUZUKI <takahiro.suzuki.ja@gmail.com>
2015-11-25 23:55:14 +0900

    refactoring

--- half_quadric_test.py
+++ half_quadric_test.py
@@ -10,6 +10,14 @@ import scipy.ndimage
 import matplotlib
 import matplotlib.pyplot as plt
 
+def plot_img(ax, img_target, title):
+    ax.imshow(img_target, cmap='gray', vmin=0, vmax=1, interpolation='nearest')
+    ax.set_title(title)
+
+def plot_diff(ax, img_target, img, title, err):
+    ax.imshow(np.abs(img_target - img), cmap='cool', vmin=-1, vmax=1, interpolation='nearest')
+    ax.set_title('{} diff (err={:.2e})'.format(title, err))
+
 def make_blur_kernel(h, w):
     # simple blur observation matrix.
     # y = Ax .. observation process.
@@ -63,29 +71,35 @@ def test_naive_inverse_problem_with_noise(img, img_blur, img_blur_noise, A, D):
     N = np.product(img.shape)
     h, w = img.shape
 
+    def mae(img_target):
+        return np.abs(img_target - img).mean()
+
     # blur+recover
     img_recover = solve2d(A, img_blur)
+    print('Blur recovery error: {}'.format(mae(img_recover)))
 
     # noise+blur, recover
     img_blur_noise_recover = solve2d(A, img_blur_noise)
+    print('Blur+Noise recovery error: {}'.format(mae(img_blur_noise_recover)))
 
     # Tikhnov regularization: minimize 1/2 ||Ax-y||_2^2 + beta*||Dx||_2^2
     # => (A'A + beta D'D)x = A'y
     #  D: regularization transform. (e.g. identity)
     beta = 0.01
     img_blur_noise_regularization_recover = recover_tikhnov_regularization(A, img_blur_noise, beta, D)
+    print('Tikhnov error: {}'.format(mae(img_blur_noise_regularization_recover)))
 
     fig, axs = plt.subplots(2, 5, figsize=(13, 5))
-    ax = axs[0, 0]; ax.imshow(img, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('org')
-    ax = axs[0, 1]; ax.imshow(img_blur, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('blur')
-    ax = axs[0, 2]; ax.imshow(img_recover, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('blur+deconv')
-    ax = axs[0, 3]; ax.imshow(np.abs(img_recover - img), cmap='cool', vmin=-1, vmax=1, interpolation='nearest'); ax.set_title('diff')
+    plot_img(axs[0, 0], img, 'org')
+    plot_img(axs[0, 1], img_blur, 'blur')
+    plot_img(axs[0, 2], img_recover, 'blur+deconv')
+    plot_diff(axs[0, 3], img_recover, img, 'deconv', mae(img_recover))
     fig.delaxes(axs[0, 4])
-    ax = axs[1, 0]; ax.imshow(img_blur_noise, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title(r'blur+noise($\sigma={:.2e}$)'.format(sigma))
-    ax = axs[1, 1]; ax.imshow(img_blur_noise_recover, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('blur+noise+recover')
-    ax = axs[1, 2]; ax.imshow(np.abs(img_blur_noise_recover - img), cmap='cool', vmin=-1, vmax=1, interpolation='nearest'); ax.set_title('diff')
-    ax = axs[1, 3]; ax.imshow(img_blur_noise_regularization_recover, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title(r'Tikhnov($\beta={:.2e}$)'.format(beta))
-    ax = axs[1, 4]; ax.imshow(np.abs(img_blur_noise_regularization_recover - img), cmap='cool', vmin=-1, vmax=1, interpolation='nearest'); ax.set_title('Tikhnov diff')
+    plot_img(axs[1, 0], img_blur_noise, r'blur+noise($\sigma={:.2e}$, err={:.2e})'.format(sigma, mae(img_blur_noise)))
+    plot_img(axs[1, 1], img_blur_noise_recover, 'blur+noise+recover')
+    plot_diff(axs[1, 2], img_blur_noise_recover, img, 'blur', mae(img_blur_noise_recover))
+    plot_img(axs[1, 3], img_blur_noise_regularization_recover, r'Tikhnov($\beta={:.2e}$)'.format(beta))
+    plot_diff(axs[1, 4], img_blur_noise_regularization_recover, img, 'Tikhnov', mae(img_blur_noise_regularization_recover))
 
 def test_half_quadratic_minimization(img, img_blur, img_blur_noise, A):
     N = np.product(img.shape)
@@ -117,22 +131,21 @@ def test_half_quadratic_minimization(img, img_blur, img_blur_noise, A):
     dphi2_0 = 0.0
     def dphi(t): return np.sign(t)
 
-    beta = 0.1
+    beta = 0.3
     img_hq = recover2d_half_quadratic(A, img_blur_noise.ravel(), beta, [D0, D1], dphi2_0, dphi).reshape(img.shape)
     print('Half-Quadratic error: {}'.format(mae(img_hq)))
 
     fig, axs = plt.subplots(2, 5, figsize=(13, 5))
-    ax = axs[0, 0]; ax.imshow(img, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('org')
-    ax = axs[0, 1]; ax.imshow(img_blur, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('blur')
-    ax = axs[0, 2]; ax.imshow(img_recover, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('blur+deconv')
-    ax = axs[0, 3]; ax.imshow(np.abs(img_recover - img), cmap='cool', vmin=-1, vmax=1, interpolation='nearest'); ax.set_title('diff (err={:.2e})'.format(mae(img_recover)))
+    plot_img(axs[0, 0], img, 'org')
+    plot_img(axs[0, 1], img_blur, 'blur')
+    plot_img(axs[0, 2], img_recover, 'blur+deconv')
+    plot_diff(axs[0, 3], img_recover, img, 'deconv', mae(img_recover))
     fig.delaxes(axs[0, 4])
-    ax = axs[1, 0]; ax.imshow(img_blur_noise, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title(r'blur+noise($\sigma={:.2e}$, err={:.2e})'.format(sigma, mae(img_blur_noise)))
-    ax = axs[1, 1]; ax.imshow(img_blur_noise_recover, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title('blur+noise+recover')
-    ax = axs[1, 2]; ax.imshow(np.abs(img_blur_noise_recover - img), cmap='cool', vmin=-1, vmax=1, interpolation='nearest'); ax.set_title('diff (err={:.2e})'.format(mae(img_blur_noise_recover)))
-    ax = axs[1, 3]; ax.imshow(img_hq, cmap='gray', vmin=0, vmax=1, interpolation='nearest'); ax.set_title(r'HQ($\beta={:.2e}$)'.format(beta))
-    ax = axs[1, 4]; ax.imshow(np.abs(img_hq - img), cmap='cool', vmin=-1, vmax=1, interpolation='nearest'); ax.set_title('HQ diff (err={:.2e})'.format(mae(img_hq)))
-
+    plot_img(axs[1, 0], img_blur_noise, r'blur+noise($\sigma={:.2e}$, err={:.2e})'.format(sigma, mae(img_blur_noise)))
+    plot_img(axs[1, 1], img_blur_noise_recover, 'blur+noise+recover')
+    plot_diff(axs[1, 2], img_blur_noise_recover, img, 'blur', mae(img_blur_noise_recover))
+    plot_img(axs[1, 3], img_hq, r'HQ($\beta={:.2e}$)'.format(beta))
+    plot_diff(axs[1, 4], img_hq, img, 'HQ', mae(img_hq))
 
 if __name__=='__main__':
     # prepare image
@@ -145,9 +158,8 @@ if __name__=='__main__':
     # simple blur observation matrix.
     A = make_blur_kernel(h, w)
 
-    # L2 regularization
+    # L2 regularization matrix.
     D = np.eye(h*w)
-
 
     # blur
     img_blur = np.dot(A, img.ravel()).reshape(img.shape)
@@ -156,7 +168,7 @@ if __name__=='__main__':
     sigma = 0.02
     img_blur_noise = img_blur + np.random.randn(*img.shape)*sigma
 
-    matplotlib.rc('font', size=8)
+    matplotlib.rc('font', size=9)
     test_naive_inverse_problem_with_noise(img, img_blur, img_blur_noise, A, D)
     test_half_quadratic_minimization(img, img_blur, img_blur_noise, A)
 
